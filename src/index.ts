5887b58e6d9fe396c2137abf5305d689132c1548
Alena Gilevskaya <alena@gilevskaya.com>
2019-10-23 19:14:30 -0700

    + run dummy

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,36 @@
-import fs from 'fs';
-import commander from 'commander';
+import fs from "fs";
+import readline from "readline";
+import commander from "commander";
 
 const program = new commander.Command();
-program.version('0.0.1');
+program.version("0.0.1");
 
-program.option('-f, --file <file>', 'run program from the .lox file');
+program.option("-f, --file <file>", "run program from the .lox file");
 program.parse(process.argv);
 
+// either running program from a file
 if (program.file) {
-  console.log(`parsing ${program.file}`);
-  fs.readFile(program.file, (err, data) => {
+  fs.readFile(program.file, (err, sourceBuff) => {
     if (err) throw err;
-    const codeLines = data.toString().split('\n');
-    console.log('lox code:', codeLines);
+    run(sourceBuff.toString());
+    process.exit(0);
   });
+  // or from REPL
 } else {
-  console.log('command promt...');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "> "
+  });
+  rl.prompt();
+  rl.on("line", line => {
+    run(line);
+    rl.prompt();
+  }).on("close", () => {
+    process.exit(0);
+  });
 }
+
+const run = (source: string) => {
+  console.log("LOX running:", source);
+};
